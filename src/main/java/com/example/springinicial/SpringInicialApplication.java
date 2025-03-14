117fc07f4c7f96be82bfe3dc58281a4bfb433db2
Pedroalano27 <alano@discente.ufg.br>
2025-03-14 09:44:49 -0300

    first commit

---
+++ src/main/java/com/example/springinicial/SpringInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInicialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInicialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinicial/SpringInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInicialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
